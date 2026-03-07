f63c84419882514ab8255bd66af1683d9a0aad87
AntigrafityMoffi <baykusbarann@gmail.com>
2026-03-07 15:50:59 +0300

    fix: add image remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+    typescript: {
+          ignoreBuildErrors: true,
+    },
+    images: {
+          remotePatterns: [
+            {
+                      protocol: 'https',
+                      hostname: '**.supabase.co',
+            },
+            {
+                      protocol: 'https',
+                      hostname: 'images.unsplash.com',
+            }
+                ],
+    },
 };
 
 export default nextConfig;
